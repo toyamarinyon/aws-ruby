d5e23dabf2b5f6f712753379420d662f7e33673b
toyama-satoshi <toyama@localhost.localdomain>
2013-08-09 21:34:57 +0900

    update hello-ec2.rb

--- hello_ec2.rb
+++ hello_ec2.rb
@@ -1,5 +1,10 @@
 require 'aws-sdk'
 
+AWS.config(
+	:access_key_id => 'AKIAJ4TTSXANZIJCLBEA',
+	:secret_access_key => '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'
+)
+
 ec2 = AWS::EC2.new
 
-ec2.intances.create(:image_id => "ami-173fbf16")
+ec2.instances.create(:image_id => "ami-173fbf16", :instance_type => "t1.micro")
